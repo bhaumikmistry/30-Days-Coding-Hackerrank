e332b5a3ab9bbf403a11fa8d5ec28fa5e775a40b
bhaumikmistry <Bhaumikpmistry@gmail.com>
2016-06-10 20:52:09 -0400

    Create day_2.py

---
+++ day_2.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+a = int(raw_input());
+b = float(raw_input());
+c = raw_input();
+
+print((i+a));
+print((d+b));
+print(s + c);
+
+# first input would be '12'
+# second input would be '4.0'
+# some string
